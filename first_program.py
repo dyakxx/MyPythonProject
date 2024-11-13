baea9933acbee1f09d1491a9034dc492ba0bc4e8
sergey d <dyakxx@gmail.com>
2024-11-13 16:00:15 +0300

    dd

---
+++ first_program.py
@@ -0,0 +1,55 @@
+# lesson 2.4
+
+a_a = int(input())
+b_b = int(input())
+
+summy = (
+    3 * ((a_a + b_b) * (a_a + b_b) * (a_a + b_b)) + 275 * (b_b * b_b) - 127 * a_a - 41
+)
+print(summy)
+
+"""
+lesson 2.3
+
+sqr_v, sqr_s = int(input()), int(input())
+v_v = sqr_v * sqr_v * sqr_v
+s_s = 6 * (sqr_s * sqr_s)
+print("Объем =", v_v)
+print("Площадь полной поверхности =", s_s)
+"""
+
+# num_a, num_b, num_c = int(input()), int(input()), int(input())
+# print(num_a, num_b, num_c, sep="\n")
+# sum_var = num_a + num_b + num_c
+# pri1nt(sum_var)
+
+
+# b = num_a + 1
+# c = b + 1
+# print(num_a, b, c, sep='\n')
+
+
+# x = 3
+# y = 4
+# z = x + y
+# z = z + 1
+# x = y
+# y = 5
+# x = z + y + 7
+
+# print(x)
+
+# =====================
+# input_sep = input()
+# input_one = input()
+# input_two = input()
+# input_three = input()
+# print(input_one, input_two, input_three, sep=input_sep)
+
+
+# 1--print()
+# name_var = input()
+# print("Привет,", name_var, end="!")
+
+# print ("I***like***Python")
+# print ('I','like','Python', sep='***')
